27755f53e30915b2980c905c67e6592e6d2c491b
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-10-01 08:36:10 +0300

    tasks rewritten as tasks, not as generic task type

--- ada-02/lab2.adb
+++ ada-02/lab2.adb
@@ -20,57 +20,61 @@ procedure Lab2 is
   package DataBase is new Data(SIZE => SIZE); use DataBase;
 
   procedure RunTasks is
-    task type GenericLabTask(cpuId, priority, storage, fnId: Integer) is
-      pragma Storage_Size (storage);
-      pragma Priority (priority);
-    end GenericLabTask;
-
-    task body GenericLabTask is
+    
+    task T1 is
+      pragma Storage_Size(32*SIZE*(SIZE + 3));
+      pragma Priority(6);
+      pragma CPU(1);
+    end T1;
+    task body T1 is
+      MA, MD: Matrix;
+      A, B, C: Vector;
     begin
-      Put_Line("Task " & Integer'Image(fnId) & " started");
-      case fnId is
-        when 1 =>
-          declare
-            MA, MD: Matrix;
-            A, B, C: Vector;
-          begin
-            GetMatrix(MA);
-            GetMatrix(MD);
-            GetVector(A);
-            GetVector(B);
-            C := Func1(A, B, MA, MD);
-            Put("C = A - B * (MA * MD) = ("); Put(C); Put_Line(")");
-          end;
-        when 2 =>
-          declare
-            MK, MM: Matrix;
-            o: Integer;
-          begin
-            GetMatrix(MK);
-            GetMatrix(MM);
-            o := Func2(MK, MM);
-            Put("o = Min(MK * MM) = "); Put(o, 2); New_Line;
-          end;
-        when 3 =>
-          declare
-            MS, MZ: Matrix;
-            W, X, T: Vector;
-          begin
-            GetMatrix(MS);
-            GetMatrix(MZ);
-            GetVector(W);
-            GetVector(X);
-            T := Func3(MS, MZ, W, X);
-            Put("T = (MS * MZ) * (W + X) = ("); Put(T); Put_Line(")");
-          end;
-        when others => null;
-      end case;
-      Put_Line("Task " & Integer'Image(fnId) & " finished");
-    end GenericLabTask;
+      Put_Line("Task T1 started");
+      GetMatrix(MA);
+      GetMatrix(MD);
+      GetVector(A);
+      GetVector(B);
+      C := Func1(A, B, MA, MD);
+      Put("C = A - B * (MA * MD) = ("); Put(C); Put_Line(")");
+      Put_Line("Task T1 finished");
+    end T1;
 
-    T1: GenericLabTask(1, 5, 32*SIZE*(SIZE + 3), 1);
-    T2: GenericLabTask(1, 5, 32*SIZE*SIZE, 2);
-    T3: GenericLabTask(1, 5, 32*SIZE*(SIZE + 2), 3);
+    task T2 is
+      pragma Storage_Size(32*SIZE*SIZE);
+      pragma Priority(6);
+      pragma CPU(2);
+    end T2;
+    task body T2 is
+      MK, MM: Matrix;
+      o: Integer;
+    begin
+      Put_Line("Task T2 started");
+      GetMatrix(MK);
+      GetMatrix(MM);
+      o := Func2(MK, MM);
+      Put("o = Min(MK * MM) = "); Put(o, 2); New_Line;
+      Put_Line("Task T2 finished");
+    end T2;
+
+    task T3 is
+      pragma Storage_Size(32*SIZE*(SIZE + 2));
+      pragma Priority(6);
+      pragma CPU(3);
+    end T3;
+    task body T3 is
+      MS, MZ: Matrix;
+      W, X, T: Vector;
+    begin
+      Put_Line("Task T3 started");
+      GetMatrix(MS);
+      GetMatrix(MZ);
+      GetVector(W);
+      GetVector(X);
+      T := Func3(MS, MZ, W, X);
+      Put("T = (MS * MZ) * (W + X) = ("); Put(T); Put_Line(")");
+      Put_Line("Task T3 finished");
+    end T3;
   begin
     null;
   end RunTasks;

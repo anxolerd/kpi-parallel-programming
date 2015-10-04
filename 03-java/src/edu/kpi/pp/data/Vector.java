b8bb1ef43e7716ee88cbe11b1314caa5a6768525
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-10-04 20:09:53 +0300

    lab3 added data package

---
+++ 03-java/src/edu/kpi/pp/data/Vector.java
@@ -0,0 +1,52 @@
+package edu.kpi.pp.data;
+
+
+import edu.kpi.pp.data.Matrix;
+
+
+public class Vector {
+
+  public static double[] ones(int size) {
+    double[] vector = new double[size];
+    for (int i = 0; i < size; i++)
+      vector[i] = 1;
+    return vector;
+  }
+
+  // return a random m-by-n matrix with values between 0 and 1
+  public static double[] random(int size) {
+    double[] vector = new double[size];
+    for (int i = 0; i < size; i++)
+      vector[i] = Math.random();
+    return vector;
+  }
+
+  // return C = A + B
+  public static double[] add(double[] left, double[] right) {
+    double[] result = new double[left.length];
+    for (int i = 0; i < left.length; i++) {
+      result[i] = left[i] + right[i];
+    }
+    return result;
+  }
+
+  // return C = A - B
+  public static double[] substract(double[] left, double[] right) {
+    double[] result = new double[left.length];
+    for (int i = 0; i < left.length; i++) {
+      result[i] = left[i] - right[i];
+    }
+    return result;
+  }
+
+  // matrix-vector multiplication (y = A * x)
+  public static double[] multiply(double[][] left, double[] right) {
+    return Matrix.multiply(left, right);
+  }
+
+  // vector-matrix multiplication (y = x^T A)
+  public static double[] multiply(double[] left, double[][] right) {
+    return Matrix.multiply(left, right);
+  }
+
+}
